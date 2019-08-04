ffd580302188e29bf1369ad58d90a9568e6a92eb
ArMaTeC <armatec0@gmail.com>
2019-08-04 15:01:01 +0100

    fixed missing language file but still dose not work

---
+++ locales/en.lua
@@ -0,0 +1,14 @@
+Locales['en'] = {
+	-- Global
+	['search'] = 'Search',
+	['confiscate_inv'] = 'Confiscate %s(%s)',
+	['inventory_label'] = 'Inventory',
+	['confiscate_weapon'] = 'Confiscate weapon',
+	['guns_label'] = 'Guns',
+	['confiscate_dirty'] = 'Confiscate dirty money ($%s)',
+}
+
+--[[
+
+%s
+--]]
